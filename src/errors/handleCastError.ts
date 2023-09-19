14f57c0a402cc225c28ee075b7a053e44c990d2e
sajjad-mugdho <sajjadmugdho@yahoo.com>
2023-09-20 02:15:29 +0600

    [Done-added-courses-model]

--- src/errors/handleCastError.ts
+++ src/errors/handleCastError.ts
@@ -1,20 +1,38 @@
-import mongoose from 'mongoose';
+import { Prisma } from '@prisma/client';
 import { IGenericErrorMessage } from '../interfaces/error';
 
-const handleCastError = (error: mongoose.Error.CastError) => {
-  const errors: IGenericErrorMessage[] = [
-    {
-      path: error.path,
-      message: 'Invalid Id',
-    },
-  ];
-
+const handleClientError = (error: Prisma.PrismaClientKnownRequestError) => {
+  let errors: IGenericErrorMessage[] = [];
+  let message = '';
   const statusCode = 400;
+
+  if (error.code === 'P2025') {
+    message = (error.meta?.cause as string) || 'Record not found!';
+    errors = [
+      {
+        path: '',
+        message,
+      },
+    ];
+  } else if (error.code === 'P2003') {
+    if (error.message.includes('delete()` invocation:')) {
+      message = 'Delete failed';
+      errors = [
+        {
+          path: '',
+          message,
+        },
+      ];
+    }
+  }
+
   return {
     statusCode,
-    message: 'Cast Error',
+    message,
     errorMessages: errors,
   };
 };
 
-export default handleCastError;
+export default handleClientError;
+
+//"//\nInvalid `prisma.semesterRegistration.delete()` invocation:\n\n\nAn operation failed because it depends on one or more records that were required but not found. Record to delete does not exist."
